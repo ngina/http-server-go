6306aa7f47d7bf18978938a9ad14deba8fcd3c0c
Ngina <ngina@users.noreply.github.com>
2024-07-17 16:14:16 -0700

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -17,12 +19,37 @@ func main() {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
+	defer l.Close()
 
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			if err != io.EOF {
+				fmt.Println("Error accepting connection: ", err.Error())
+				os.Exit(1)
+			}
+			fmt.Println("Received EOF: Connection closed by client. Exiting...")
+			break
+		}
+		defer conn.Close()
+		fmt.Println("Connection accepted")
+
+		// Read first request and respond with 200 OK
+		request := make([]byte, 1024)
+		readBytes, err := conn.Read(request)
+		if err != nil {
+			fmt.Println("Error reading request: ", err.Error())
+			os.Exit(1)
+		}
+		fmt.Println("Received request:\n", string(request[:readBytes]))
+		fmt.Printf("Split request using empty spaces: %s\n", strings.Split(string(request[:readBytes]), " "))
+
+		// Get path and respond with 200 OK or 404 Not Found if path is "/"
+		path := strings.Split(string(request[:readBytes]), " ")[1]
+		if path == "/" {
+			conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+		} else {
+			conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+		}
 	}
-
-	conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
 }
