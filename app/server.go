1612f9d99a83340fdde9dc99cce68162ea9158a7
Ngina <ngina@users.noreply.github.com>
2024-07-21 23:05:04 -0700

    Fix server to receive multiple connections

--- app/server.go
+++ app/server.go
@@ -8,6 +8,66 @@ import (
 	"strings"
 )
 
+func handleConnection(conn net.Conn) {
+	// Create a buffer to hold incoming requests
+	request := make([]byte, 1024)
+	fmt.Println("Waiting for request...")
+
+	readBytes, err := conn.Read(request)
+	if err != nil {
+		if err != io.EOF {
+			fmt.Println("Error reading request:", err.Error())
+			os.Exit(1)
+		}
+		fmt.Println("Received EOF: Connection closed by client. Exiting...")
+		return
+	}
+
+	// Get path and respond with 200 OK or 404 Not Found if path is "/"
+	fmt.Println("Received request:", string(request[:readBytes]))
+	path := strings.Split(string(request[:readBytes]), " ")[1]
+
+	requestHeaders := strings.Split(string(request[:readBytes]), "\r\n")
+	fmt.Println("Request strings split by \\r\\n:", requestHeaders, len(requestHeaders))
+	for str := range requestHeaders {
+		fmt.Println(requestHeaders[str])
+	}
+
+	const okStatusLine = "HTTP/1.1 200 OK\r\n"
+	if path == "/" {
+		headers := "\r\n"
+		conn.Write([]byte(okStatusLine + headers))
+
+	} else if strings.HasPrefix(path, "/echo") {
+		body := strings.Split(path[1:], "/")[1] //todo: validate path is /echo/<body>
+		fmt.Println("Echoing body:", body, len(body))
+		response := fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(body), body)
+		conn.Write([]byte(response))
+
+	} else if strings.HasPrefix(path, "/user-agent") {
+		headers := requestHeaders[1:4]
+		var userAgentHeader string
+		for _, header := range headers {
+			if strings.HasPrefix(header, "User-Agent") { //todo: validate header is User-Agent
+				userAgentHeader = header
+				break
+			}
+		}
+		body := strings.Split(userAgentHeader, ": ")[1]
+		fmt.Println("Echoing body:", body)
+
+		responseHeadersAndBody := fmt.Sprintf("Content-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(body), body)
+		response := okStatusLine + responseHeadersAndBody
+		conn.Write([]byte(response))
+
+	} else {
+		conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+	}
+	fmt.Println("Sent response")
+	conn.Close()
+	fmt.Println("Connection closed")
+}
+
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
@@ -21,66 +81,17 @@ func main() {
 	defer l.Close()
 
 	// Listen for connections
-	conn, err := l.Accept()
-	if err != nil {
-		if err != io.EOF {
-			fmt.Println("Error accepting connection: ", err.Error())
-			os.Exit(1)
-		}
-		fmt.Println("Received EOF: Connection closed by client. Exiting...")
-	}
-	defer conn.Close()
-	fmt.Println("Connection accepted")
-
-	// Create a buffer to hold incoming requests
-	request := make([]byte, 1024)
-	fmt.Println("Waiting for request...")
 	for {
-		readBytes, err := conn.Read(request)
+		conn, err := l.Accept()
 		if err != nil {
-			fmt.Println("Error reading request:", err.Error())
-			os.Exit(1)
-		}
-		fmt.Println("Received request:", string(request[:readBytes]))
-
-		// Get path and respond with 200 OK or 404 Not Found if path is "/"
-		path := strings.Split(string(request[:readBytes]), " ")[1]
-
-		requestHeaders := strings.Split(string(request[:readBytes]), "\r\n")
-		fmt.Println("Request strings:", requestHeaders, len(requestHeaders))
-		for str := range requestHeaders {
-			fmt.Println(requestHeaders[str])
-		}
-
-		const okStatusLine = "HTTP/1.1 200 OK\r\n"
-		if path == "/" {
-			headers := "\r\n"
-			conn.Write([]byte(okStatusLine + headers))
-
-		} else if strings.HasPrefix(path, "/echo") {
-			body := strings.Split(path[1:], "/")[1] //todo: validate path is /echo/<body>
-			fmt.Println("Echoing body:", body, len(body))
-			response := fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(body), body)
-			conn.Write([]byte(response))
-
-		} else if strings.HasPrefix(path, "/user-agent") {
-			headers := requestHeaders[1:4]
-			var userAgentHeader string
-			for _, header := range headers {
-				if strings.HasPrefix(header, "User-Agent") { //todo: validate header is User-Agent
-					userAgentHeader = header
-					break
-				}
+			if err != io.EOF {
+				fmt.Println("Error accepting connection: ", err.Error())
+				os.Exit(1)
 			}
-			body := strings.Split(userAgentHeader, ": ")[1]
-			fmt.Println("Echoing body:", body)
-
-			responseHeadersAndBody := fmt.Sprintf("Content-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(body), body)
-			response := okStatusLine + responseHeadersAndBody
-			conn.Write([]byte(response))
-
-		} else {
-			conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+			fmt.Println("Received EOF: Connection closed by client. Exiting...")
+			break
 		}
+		fmt.Println("Connection accepted")
+		handleConnection(conn)
 	}
 }
